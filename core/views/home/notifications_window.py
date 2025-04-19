6cd6b97df2fdcc652e87a8d31f1112c3695b4824
FelipeAvelno <felipe.avelino10101@gmail.com>
2025-04-19 19:32:14 -0300

    add btn de retorno

--- core/views/home/notifications_window.py
+++ core/views/home/notifications_window.py
@@ -1,14 +1,15 @@
-from PyQt5.QtWidgets import (QWidget, QVBoxLayout, QLabel, QListWidget, QListWidgetItem, QMessageBox)
+from PyQt5.QtWidgets import (QWidget, QVBoxLayout, QLabel, QListWidget, QListWidgetItem, QMessageBox, QPushButton)
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QShowEvent
 from core.controllers.auth.auth_controller import AuthController
 from core.controllers.screens_controller import ScreensController
 
 class NotificationsWindow(QWidget):
-    def __init__(self, vehicles_controller, auth_controller):
+    def __init__(self, vehicles_controller, auth_controller, screens_controller: ScreensController):
         super().__init__()
         self.vehicles_controller = vehicles_controller
         self.auth_controller = auth_controller
+        self.screens_controller = screens_controller
         self.init_ui()
 
     def init_ui(self):
@@ -24,6 +25,10 @@ class NotificationsWindow(QWidget):
 
         self.notifications_list = QListWidget()
         layout.addWidget(self.notifications_list)
+
+        self.back_button = QPushButton("Voltar para Home")
+        self.back_button.clicked.connect(self.go_to_home)
+        layout.addWidget(self.back_button)
 
         self.setLayout(layout)
 
@@ -57,3 +62,6 @@ class NotificationsWindow(QWidget):
         """Carrega notificações quando a tela for exibida."""
         super().showEvent(event)
         self.load_notifications()
+
+    def go_to_home(self):
+        self.screens_controller.set_screen("home")
